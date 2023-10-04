5f63e85db238c504d3a49ed81a41acf8eafc55a8
Tricia Sawyer <tricia.sawyer03@gmail>
2023-10-04 14:56:27 -0500

    add responsiveness

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -3,7 +3,7 @@
     <footer class="footer">
       <p class="footer-contact">Contact: (123)456-7890</p>
       <p class="footer-location">Location: 1258 N 49th St. Seattle, WA</p>
-      <p class="footer-hours">Hours: 6am to 7pm</p>
+      <p class="footer-hours">Hours: 6am-7pm</p>
     </footer>
   </div>
 </template>
